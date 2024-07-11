d99612240369be564c18c1592096be6dd7308891
fernandograde <fernandoclbf@usp.br>
2024-07-10 21:18:15 -0300

    Obtaining mydos/ directory from SYSeg

---
+++ mydos/logo.c
@@ -0,0 +1,43 @@
+/*
+ *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2024 fernandograde <fernandoclbf@usp.br>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative work from SYSeg (https://gitlab.com/monaco/syseg)
+ *  and contains modifications carried out by the following author(s):
+ *  fernandograde <fernandoclbf@usp.br>
+ */
+
+/* A (rather unimpressive) logo for the splash screen. */ 
+
+const char logo[] =
+  "                                                                                " /*  0 */
+  "                                                                                " /*  1 */
+  "                                                                                " /*  2 */
+  "                                                                                " /*  3 */
+  "                                                                                " /*  4 */
+  "                                                                                " /*  6 */
+  "                                                                                " /*  5 */
+  "                                                                                " /*  6 */
+  "                                                                                " /*  7 */
+  "           #####    #####           ########        #####      #####            " /*  8 */
+  "            ####   ####              ###   ###    ##   ###   ##    ##           " /*  9 */
+  "            # ###  # ##   ###   ###  ###   ###  ###     ###  ###                " /* 10 */
+  "           ## ### #  ##    ###  ##   ###   ###  ###     ###   #####             " /* 11 */
+  "           #   ####  ##    ###  ##   ###   ###  ###    ###       ###            " /* 12 */
+  "          ##   ###   ##     ## ##   ###   ###   ###    ##   ##    ##            " /* 13 */
+  "         ####  ##  ######   ###    ########       #####     ######              " /* 14 */
+  "                            ##                                                  " /* 15 */
+  "                        ####                                                    " /* 16 */
+  "                                                                                " /* 17 */
+  "                                                                                " /* 18 */
+  "                                                                                " /* 19 */
+  "                                                                                " /* 20 */
+  "                                                                                " /* 21 */
+  "                                                                                " /* 22 */
+  "                                                                                " /* 23 */
+  "                                                                                " /* 22 */
+  "                                                                                " /* 23 */
+  ;
+  
